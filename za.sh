5368e4d30b0d259fdb9977e28e4fa8a603f2da06
ikogendo <103246068+ikogendo@users.noreply.github.com>
2025-06-03 18:03:23 +0300

    Create za.sh

---
+++ za.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.0+ubuntu24.04_all.deb
+dpkg -i zabbix-release_latest_7.0+ubuntu24.04_all.deb
+apt update && apt install zabbix-agent2 -y 
