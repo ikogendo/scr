458a67b7cdf94c0530c9e10686b89244bf859f21
ikogendo <103246068+ikogendo@users.noreply.github.com>
2025-08-12 18:08:26 +0300

    Delete kubspey.sh

--- kubspey.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-SRC_IP=$1;SRC_IP="${SRC_IP:-source.ip}"
-if ! command -v pip3 install &> /dev/null; then apt install python3 python3-pip -y ; fi
-git clone https://github.com/kubernetes-sigs/kubespray;cd kubespray/; pip3 install -r requirements.txt; cp -pr inventory/sample inventory/local
-declare -a IPS=($(cat $SRC_IP))
-CONFIG_FILE=inventory/local/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
-find ./ -name  k8s-cluster.yaml -exec sed -i -e"/kubeconfig_localhost:/d" {} ; echo "kubeconfig_localhost: true" >> {} \;
